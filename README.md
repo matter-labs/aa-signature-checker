00a3428bc2191f7aca7dc93b8a36a8a500b05df4
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2022-06-27 10:08:21 +0200

    Add support for EIP1271

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A library for convenient, universal signature checking for zkSync 2.0.
 
-Currently it only implement ECDSA signature verification, but in the future it will support EIP1271 as well.
+Currently, it is a copy of the OpenZeppelin's [SignatureChecker](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/74738721dc9cfa820687f6b700d2583b16a21c0d/contracts/utils/cryptography/SignatureChecker.sol#L17) library. In the future, it will be slightly edited to avoid emitting warnings about `ecrecover` during compilation.
--- contracts/SignatureChecker.sol
+++ contracts/SignatureChecker.sol
@@ -1,18 +1,26 @@
 pragma solidity ^0.8.0;
 
 import "./openzeppelin/ECDSA.sol";
+import "./openzeppelin/IERC1271.sol";
 
 library SignatureChecker {
     /** 
      * @dev Checks the signature of an account using ecrecover.
      * In the future, it will use EIP-1271 to validate signatures of AA contracts.
      */
-    function checkSignature(
-        address _address,
-        bytes32 _hash,
-        bytes memory _signature
-    ) pure internal returns (bool) {
-        (address recoveredAddress, ECDSA.RecoverError recoverError) = ECDSA.tryRecover(_hash, _signature);
-        return recoverError == ECDSA.RecoverError.NoError && recoveredAddress == _address;
+    function isValidSignatureNow(
+        address signer,
+        bytes32 hash,
+        bytes memory signature
+    ) internal view returns (bool) {
+        (address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(hash, signature);
+        if (error == ECDSA.RecoverError.NoError && recovered == signer) {
+            return true;
+        }
+
+        (bool success, bytes memory result) = signer.staticcall(
+            abi.encodeWithSelector(IERC1271.isValidSignature.selector, hash, signature)
+        );
+        return (success && result.length == 32 && abi.decode(result, (bytes4)) == IERC1271.isValidSignature.selector);
     }
 }
